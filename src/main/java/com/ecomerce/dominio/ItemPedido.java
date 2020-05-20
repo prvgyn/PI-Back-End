af38cc8a95e1f9efee092cd06460f57f70284e44
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-05-20 12:28:18 -0300

    ItemPedido, ManyToMany associação com atributos extras / 1

---
+++ src/main/java/com/ecomerce/dominio/ItemPedido.java
@@ -0,0 +1,101 @@
+package com.ecomerce.dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.ecomerce.dominio.pk.ItemPedidoPK;
+
+@Entity
+@Table(name = "tb_pedido_produto")
+public class ItemPedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemPedidoPK id;
+
+	private Integer qtd;
+	private Double preco;
+	private Double desconto;
+
+	public ItemPedido() {
+
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Integer qtd, Double preco, Double desconto) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.qtd = qtd;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
+	public Integer getQtd() {
+		return qtd;
+	}
+
+	public void setQtd(Integer qtd) {
+		this.qtd = qtd;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
