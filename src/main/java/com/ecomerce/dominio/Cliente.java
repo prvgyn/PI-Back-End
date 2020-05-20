b5527ff0317af4d8319c797aa45e3be9a7448484
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-05-19 23:03:22 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/ecomerce/dominio/Cliente.java
+++ src/main/java/com/ecomerce/dominio/Cliente.java
@@ -1,14 +1,21 @@
 package com.ecomerce.dominio;
 
 import java.io.Serializable;
-import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import java.sql.Date;
 
 // import com.ecomerce.enumerador.TipoCliente;
-
+@Entity
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	//Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String sobrenome;
