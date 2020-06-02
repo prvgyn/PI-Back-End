b2e64ca8ab4f7ada085706919df1c45cdc5c740c
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-06-02 20:56:49 -0300

    Spring Security - Concluído

--- src/main/java/com/ecomerce/dominio/Usuario.java
+++ src/main/java/com/ecomerce/dominio/Usuario.java
@@ -68,7 +68,7 @@ public class Usuario implements UserDetails{
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return roles;
 	}
 
 	@Override
